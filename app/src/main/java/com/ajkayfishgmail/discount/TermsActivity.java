b173b9de365b3ae9a08a4158b35c8b10b839af48
Kloranthy <weigelj@mail.gvsu.edu>
2015-04-19 20:56:22 -0400

    made a legal/about page. someday it will launch on the first use and after updates to the terms. for now it is launched via menu.

---
+++ app/src/main/java/com/ajkayfishgmail/discount/TermsActivity.java
@@ -0,0 +1,26 @@
+package com.ajkayfishgmail.discount;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class TermsActivity extends ActionBarActivity
+{
+    // SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+
+        /*prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ped = prefs.edit();
+        ped.putInt("versionAccepted", 1); // put version # here
+        ped.apply();*/
+    }
+}
